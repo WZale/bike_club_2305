48c311ed7d33b1a773e878f9e0a7124ffe895bd1
William Zale <williamzale@gmail.com>
2023-06-05 13:58:28 -0400

    feat: Add initial version of best_time(ride) method

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -12,15 +12,38 @@ class BikeClub
     @bikers << biker
   end
 
+  # from Dani
   def most_rides
-    rides = {}
-    @bikers.each do |biker| 
-      rides[biker.name] = biker.rides.values.flatten
+    @bikers.max_by do |biker|
+      biker.rides.sum do |ride, times|
+        times.count 
+      end
     end
-    max_rider = rides.max_by do |biker, rides|
-      biker
-    end
-    max_rider[0]
   end
+
+  # I think this one doesn't quite work because @rider_2 can't ride
+  # the terrain of @ride_1, or something? Not sure.
+  def best_time(ride)
+    a = @bikers.select do |biker|
+      require 'pry'; binding.pry
+      biker.rides[ride].min
+    end
+    a
+  end
+
+
+  # My version, which only returns the biker's name and not
+  # the actual biker object
+
+  # def most_rides
+  #   rides = {}
+  #   @bikers.each do |biker| 
+  #     rides[biker.name] = biker.rides.values.flatten
+  #   end
+  #   max_rider = rides.max_by do |biker, rides|
+  #     biker
+  #   end
+  #   max_rider[0]
+  # end
 
 end
